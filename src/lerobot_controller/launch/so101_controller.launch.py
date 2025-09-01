ebbd557a6836e8aeb2fd03f213f4226c62a7ae4f
XiaoqiangWu <wuxiaoqiang.rtos@huawei.com>
2025-09-01 11:53:06 +0800

    update fixed frame and fit to ROS humble.

--- src/lerobot_controller/launch/so101_controller.launch.py
+++ src/lerobot_controller/launch/so101_controller.launch.py
@@ -44,7 +44,7 @@ def generate_launch_description():
         executable="ros2_control_node",
         parameters=[
             {"robot_description": robot_description,
-             "use_sim_time": is_sim},
+             "use_sim_time": LaunchConfiguration("is_sim", default="true")},
             os.path.join(
                 get_package_share_directory("lerobot_controller"),
                 "config",
