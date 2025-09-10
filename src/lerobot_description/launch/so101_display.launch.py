ac083f87d658efc6cd10b5d266cb46ca24f2da0b
XiaoqiangWu <wuxiaoqiang.rtos@huawei.com>
2025-09-10 16:53:42 +0800

    add using sim time argument in display launch file

--- src/lerobot_description/launch/so101_display.launch.py
+++ src/lerobot_description/launch/so101_display.launch.py
@@ -3,6 +3,7 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
 from launch.substitutions import Command, LaunchConfiguration
 
 from launch_ros.actions import Node
@@ -10,6 +11,15 @@ from launch_ros.parameter_descriptions import ParameterValue
 
 
 def generate_launch_description():
+    # Check if we're told to use sim time
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    
+    # New parameter to control whether to show GUI sliders or use real robot
+    use_gui = LaunchConfiguration('use_gui')
+    
+    # Parameter for which joint_states topic to use
+    joint_states_topic = LaunchConfiguration('joint_states_topic')
+
     maxarm_description_dir = get_package_share_directory("lerobot_description")
 
     model_arg = DeclareLaunchArgument(
@@ -22,12 +32,17 @@ def generate_launch_description():
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
-        parameters=[{"robot_description": robot_description}]
+        output='screen',
+        parameters=[{"robot_description": robot_description}],
+        remappings=[('/joint_states', joint_states_topic)]
     )
 
     joint_state_publisher_gui_node = Node(
         package="joint_state_publisher_gui",
-        executable="joint_state_publisher_gui"
+        executable="joint_state_publisher_gui",
+        name='joint_state_publisher_gui',
+        remappings=[('/joint_states', '/so101_follower/joint_commands')],
+        condition=IfCondition(use_gui)
     )
 
     rviz_node = Node(
@@ -39,6 +54,21 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use sim time if true'),
+            
+        DeclareLaunchArgument(
+            'use_gui',
+            default_value='true',
+            description='Whether to show joint_state_publisher_gui sliders'),
+            
+        DeclareLaunchArgument(
+            'joint_states_topic',
+            default_value='/joint_states',
+            description='Topic to get joint states from'),
+
         model_arg,
         joint_state_publisher_gui_node,
         robot_state_publisher_node,
